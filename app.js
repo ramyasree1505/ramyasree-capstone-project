885893f90b0a8bc0c3556fa3eea9c1beef825a81
ramyasree1505 <ramyasreeslr@gmail.com>
2025-10-25 16:48:54 +0530

    New web Application for devops

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("views"));
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "success", message: "App is running fine 💚" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
